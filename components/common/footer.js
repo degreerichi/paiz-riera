fd13d1ae0b0bfb12144aa3efd44b37086697115c
Rychiv4 <rick_valladares@hotmail.com>
2021-03-17 14:56:25 -0600

    agregando footer

--- components/common/footer.js
+++ components/common/footer.js
@@ -4,6 +4,13 @@ const Footer = ()=>{
     return (
         <footer>
             <img src="/paiz-riera-full-logo.svg" alt="" className="footer-logo"/>
+            <h1 className="no-before mt-4">contacto@paizriera.com</h1>
+            <br/>
+            <span>Av. Reforma 6-39 zona 10</span>
+            <span>Centro Corporativo Guayacán</span>
+            <span>Guatemala, ciudad</span>
+            <br/>
+            <h3 className="no-before">+1 (917) 123 456  +1 (917) 333 987</h3>
         </footer>
     )
 }
